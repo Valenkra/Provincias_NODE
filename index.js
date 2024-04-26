890f2580ffe74e5e69af3b7c2a3ca5aa6365f453
VALENTINA ORIT VUGIN <47866545@ort>
2024-04-26 10:13:20 -0300

    Todo terminado

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ provincias.push(new Province(8, "Formosa", "Provincia de Formosa", -26.1852, -58
 provincias.push(new Province(9, "Jujuy", "Provincia de Jujuy", -24.1858, -65.2995, 9));
 let cantId = provincias.length;
 
-//1
+//1 TERMINADO
 app.get("/api/province", (req, res) => {
     res.status(200).send(provincias);
 });
 
-//2
+//2 TERMINADO
 app.get("/api/province/:id", (req, res) => {
     let id = req.params.id;
     if(validacionesHelper.getIntegerOrDefault(id, -1) != -1) {
@@ -43,32 +43,65 @@ app.get("/api/province/:id", (req, res) => {
 });
 
 
-//3
+//3 TERMINADO
 app.post("/api/province", (req, res) => {
     let name = req.query.name;
     let fullName = req.query.fullName;
     let latitude = req.query.latitude;
     let longitude = req.query.longitude;
     let displayOrder = req.query.displayOrder;
-    
+
     if(validacionesHelper.getIntegerOrDefault(latitude, NaN) != NaN && validacionesHelper.getIntegerOrDefault(longitude, NaN) != NaN
-        && validacionesHelper.getStringOrDefault(name, null) != null && validacionesHelper.getStringOrDefault(fullName, null) != null &&
-        (displayOrder == "" || displayOrder == null || validacionesHelper.getIntegerOrDefault(displayOrder, NaN) != NaN))
-    res.status(200).send('');
+        && validacionesHelper.getStringOrDefault(name, null) != null && validacionesHelper.getStringOrDefault(fullName, null) != null){
+            cantId++;
+            if(validacionesHelper.getStringOrDefault(displayOrder,null) != null && isNaN(validacionesHelper.getIntegerOrDefault(displayOrder, NaN))){
+                displayOrder = null;
+            }
+            provincias.push(new Province(cantId, name, fullName, latitude, longitude, displayOrder));
+            return res.status(201).send('Provincia creada satisfactoriamente.');
+        }
+    else{
+        return res.status(400).send("Todos los parametros deben ser validos.");
+    }
 });
 
-
+//4 TERMINADO
 app.put("/api/province", (req, res) => {
+    let id = req.query.id;
     let name = req.query.name;
     let fullName = req.query.fullName;
     let latitude = req.query.latitude;
     let longitude = req.query.longitude;
     let displayOrder = req.query.displayOrder;
 
+    if(validacionesHelper.getIntegerOrDefault(id, -1) != -1) {
+        let i = provincias.findIndex(p => p.id == id);
+        if(i != -1){
+            if(validacionesHelper.getStringOrDefault(name, null) != null && name.length >= 3) provincias[i].name = name;
+            if(validacionesHelper.getStringOrDefault(fullName, null) != null && fullName.length >= 3) provincias[i].fullName = fullName;
+            if(validacionesHelper.getIntegerOrDefault(latitude, NaN) != NaN && validacionesHelper.getStringOrDefault(latitude, null) != null) provincias[i].latitude = latitude;
+            if(validacionesHelper.getIntegerOrDefault(longitude, NaN) != NaN && validacionesHelper.getStringOrDefault(longitude, null) != null) provincias[i].longitude = latitude;
+            if(validacionesHelper.getIntegerOrDefault(longitude, NaN) != NaN && validacionesHelper.getStringOrDefault(longitude, null) != null) 
+            provincias[i].displayOrder = displayOrder;
 
-    res.status(200).send('');
+            return res.status(201).send("Provincia actualizada satisfactoriamente.");
+        }
+        else return res.status(404).send("No existe provincia con ese id.");
+    }else return res.status(400).send("Todos los parametros deben ser validos.");
 });
 
+//5 TERMINADO
+app.delete("/api/province/:id", (req, res) => {
+    let id = req.params.id;
+    if(validacionesHelper.getIntegerOrDefault(id, -1) != -1) {
+        let i = provincias.findIndex(p => p.id == id);
+        if(i != -1){
+            provincias.splice(i,1);
+            return res.status(200).send("Provincia eliminada satisfactoriamente.");
+        }
+        else return res.status(400).send("No se encontró la provincia.");
+    }else return res.status(400).send("Los parametros no son validos.");
+});
 
 
 
